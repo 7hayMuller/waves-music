257f75e82fc93f2b262c9e570c9519d5ee90be59
7hayMuller <thaynamuller88@gmail.com>
2021-03-25 02:03:55 +0000

    Adding buttons and style to the main screen

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/app.scss'
+import Player from './components/Player';
+import Song from './components/Song';
+
+function App() {
+  return (
+    <div className="App">
+      <Song/>
+      <Player/>      
+      </div>    
+  );
+}
+
+export default App;
