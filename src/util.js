fea82985286e239dce1103692db0acdaec2ebe2c
7hayMuller <thaynamuller88@gmail.com>
2021-03-27 23:25:29 +0000

    Adding data to the playlist and styling components

---
+++ src/util.js
@@ -0,0 +1,36 @@
+import {v4 as uuidv4} from 'uuid';
+
+function chillhop() {
+    return [
+        {
+            name:'Cascade',
+            cover: "https://chillhop.com/wp-content/uploads/2021/02/70a56749b8b89815fa75446030c6ba57d2c34de7-1024x1024.jpg",
+            artist:'Knowmadic',
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=13091",
+            color:['#205950', '#2ab3bf'],
+            id: uuidv4(),
+            active:false
+        },
+        {
+            name:'Sunrise Hike',
+            cover: "https://chillhop.com/wp-content/uploads/2021/02/70a56749b8b89815fa75446030c6ba57d2c34de7-1024x1024.jpg",
+            artist:'Aso, Middle School, Aviino',
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=13091",
+            color:['#205950', '#2ab3bf'],
+            id: uuidv4(),
+            active:false
+
+        },
+        {
+            name:'Beaver Creek',
+            cover:"https://chillhop.com/wp-content/uploads/2021/02/70a56749b8b89815fa75446030c6ba57d2c34de7-1024x1024.jpg",
+            artist:'Aso, Middle School, Aviino',
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=13091",
+            color:['#205950', '#2ab3bf'],
+            id: uuidv4(),
+            active:false
+        }        
+    ]
+}
+
+export default chillhop;
